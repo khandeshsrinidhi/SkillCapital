c4f4e0b6c0339368d4874360b441fb5e547f326e
Giri Yaram <60086844+giriyaram@users.noreply.github.com>
2024-08-06 17:48:29 +0530

    Update CorsConfig.java
    imported value annotation

--- Backend/Skill_Capital/src/main/java/com/dl/config/CorsConfig.java
+++ Backend/Skill_Capital/src/main/java/com/dl/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.dl.config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CorsFilter;
