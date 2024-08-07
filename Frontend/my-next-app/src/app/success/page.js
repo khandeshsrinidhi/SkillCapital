3b841daf3ff5b5155e8bcfac218cad59d68aa6d2
DivyaTech29 <nadavapallisaidivya@gmail.com>
2024-08-07 18:41:40 +0530

    Add frontend folder from nikhil branch

---
+++ Frontend/my-next-app/src/app/success/page.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="max-w-md w-full mx-auto bg-white shadow-md rounded-md p-4">
+            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <strong className="font-bold">Success!</strong>
+            <span className="block sm:inline"> Login successful.</span>
+            </div>
+      </div>
+    </div>
+  );
+}
